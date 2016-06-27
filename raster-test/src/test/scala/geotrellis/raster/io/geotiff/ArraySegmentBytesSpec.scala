3188e92a2d7a610e7b5ed935d20b53168f70bcd1
MyNameIsBouf <jbouffard1993@gmail.com>
2016-06-27 16:19:07 -0400

    Began writing tests

---
+++ raster-test/src/test/scala/geotrellis/raster/io/geotiff/ArraySegmentBytesSpec.scala
@@ -0,0 +1,15 @@
+package geotrellis.raster.io.geotiff
+
+import geotrellis.raster._
+import geotrellis.raster.io.geotiff.reader._
+import geotrellis.raster.testkit._
+
+import org.scalatest._
+
+class ArraySegmentBytesSpec extends FunSpec 
+  with Matchers
+  with BeforeAndAfterAll
+  with RasterMatchers
+  with GeoTiffTestUtils
+  with TileBuilders {
+  }
