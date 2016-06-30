a7becd125870acd094508d0427ee525240125576
MyNameIsBouf <jbouffard1993@gmail.com>
2016-06-30 16:25:03 -0400

    Made chnages

--- raster/src/main/scala/geotrellis/raster/io/geotiff/SegmentBytes.scala
+++ raster/src/main/scala/geotrellis/raster/io/geotiff/SegmentBytes.scala
@@ -1,14 +1,11 @@
 package geotrellis.raster.io.geotiff
 
-import scala.collection.mutable._
 /**
  * The base trait of CompressedBytes. It can be implemented either as
  * an Array[Array[Byte]] or as a Stream that is lazily read in.
  */
 trait SegmentBytes {
   val size: Int
-  val intersectingSegments: Set[Int]
-
   def getSegment(i: Int): Array[Byte]
   def map(f: Array[Byte] => Int): Seq[Int]
 }
