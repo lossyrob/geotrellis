af214ec51ad0ef2dd4159d3da55d405cb15e7a86
lossyrob <rdemanuele@gmail.com>
2016-11-10 19:57:30 -0500

    Fixed bug in merge logic.

--- raster-test/src/test/scala/geotrellis/raster/merge/TileMergeMethodsSpec.scala
+++ raster-test/src/test/scala/geotrellis/raster/merge/TileMergeMethodsSpec.scala
@@ -12,7 +12,7 @@ class TileMergeMethodsSpec extends FunSpec
     with RasterMatchers {
   describe("SinglebandTileMergeMethods") {
 
-    it("should merge prototype for each cell type") {
+    it("should merge smaller prototype tile for each cell type") {
       val cellTypes: Seq[CellType] =
         Seq(
           BitCellType,
@@ -56,7 +56,7 @@ class TileMergeMethodsSpec extends FunSpec
       }
     }
 
-    it("should merge offset prototype for each cell type") {
+    it("should merge larger  prototype for each cell type") {
       val cellTypes: Seq[CellType] =
         Seq(
           BitCellType,
@@ -102,6 +102,103 @@ class TileMergeMethodsSpec extends FunSpec
         val merged = proto.merge(smallerExtent, largerExtent, tile)
         withClue(s"Failing on cell type $ct: ") {
           assertEqual(merged, expected)
+        }
+      }
+    }
+
+    it("should merge sprototype for each cell type") {
+      val cellTypes: Seq[CellType] =
+        Seq(
+          BitCellType,
+          ByteCellType,
+          ByteConstantNoDataCellType,
+          ByteUserDefinedNoDataCellType(1.toByte),
+          UByteCellType,
+          UByteConstantNoDataCellType,
+          UByteUserDefinedNoDataCellType(1.toByte),
+          ShortCellType,
+          ShortConstantNoDataCellType,
+          ShortUserDefinedNoDataCellType(1.toShort),
+          UShortCellType,
+          UShortConstantNoDataCellType,
+          UShortUserDefinedNoDataCellType(1.toShort),
+          IntCellType,
+          IntConstantNoDataCellType,
+          IntUserDefinedNoDataCellType(1),
+          FloatCellType,
+          FloatConstantNoDataCellType,
+          FloatUserDefinedNoDataCellType(1.0f),
+          DoubleCellType,
+          DoubleConstantNoDataCellType,
+          DoubleUserDefinedNoDataCellType(1.0)
+        )
+
+      for(ct <- cellTypes) {
+        val arr = Array.ofDim[Double](100).fill(5.0)
+        arr(50) = 1.0
+        arr(55) = 0.0
+        arr(60) = Double.NaN
+
+        val tile =
+          DoubleArrayTile(arr, 10, 10, DoubleCellType).convert(ct)
+
+        val proto = tile.prototype(ct, tile.cols, tile.rows)
+        val merged = proto merge tile
+        withClue(s"Failing on cell type $ct: ") {
+          assertEqual(merged, tile)
+        }
+      }
+    }
+
+    it("should merge 2 tiles into a larger tile for NoNoData cell types") {
+
+      val cellTypes: Seq[CellType] =
+        Seq(
+          BitCellType,
+          ByteCellType,
+          UByteCellType,
+          ShortCellType,
+          UShortCellType,
+          IntCellType,
+          FloatCellType,
+          DoubleCellType
+        )
+
+      for(ct <- cellTypes) {
+        val tile1 =
+          createTile(
+            Array(0.0, 2.0,
+                  3.0, 4.0), 2, 2).convert(ct)
+
+        val tile2 =
+          createTile(
+            Array(5.0, 6.0,
+                  7.0, 0.0), 2, 2).convert(ct)
+
+        val largerTile =
+          createTile(
+            Array.ofDim[Double](16), 4, 4
+          ).convert(ct)
+
+        val e1 = Extent(0, 2, 2, 4)
+        val e2 = Extent(2, 2, 4, 4)
+        val largeE = Extent(0, 0, 4, 4)
+
+        val expected =
+          createTile(
+            Array(0.0, 2.0, 5.0, 6.0,
+                  3.0, 4.0, 7.0, 0.0,
+                  0.0, 0.0, 0.0, 0.0,
+                  0.0, 0.0, 0.0, 0.0), 4, 4).convert(ct)
+
+        val actual =
+          largerTile
+            .mapDouble { x => x }
+            .merge(largeE, e1, tile1)
+            .merge(largeE, e2, tile2)
+
+        withClue(s"Failing on cell type $ct: ") {
+          assertEqual(actual, expected)
         }
       }
     }
